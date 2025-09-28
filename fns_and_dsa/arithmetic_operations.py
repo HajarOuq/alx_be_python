ce20e01c1e0e02525eb9706d8580512ef86484d0
HajarOuq <hajar.ouqacem@gmail.com>
2025-09-28 19:11:05 +0100

    fns and dsa

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
     match operation:
-        case 'add':
+        case "add":
             result = num1 + num2
-        case 'substract':
+        case "substract":
             result = num1 - num2
-        case 'multiply':
+        case "multiply":
             result = num1 * num2
-        case 'divide':
+        case "divide":
             if num2 == 0:
                 result = "Impossible"
             else:
